98fc62b37a6e6c9e752f2ffda279e16de208bdb4
95jinhong <95park@naver.com>
2024-02-13 15:15:02 +0900

    fix: output ec2 resource 제거

--- ec2.tf
+++ ec2.tf
@@ -7,10 +7,10 @@ provider "aws" {
 #   instance_type = "t2.micro"
 # }
 
-output "ec2_ip" {
-    value = aws_instance.ec2.private_ip
-}
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.ec2.public_ip
-}
+# output "ec2_ip" {
+#     value = aws_instance.ec2.private_ip
+# }
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.ec2.public_ip
+# }
